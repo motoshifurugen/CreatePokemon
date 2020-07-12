35d0a65efced91f93c4df4569837a2ea8ba65b29
motoshifurugen <amanogawa_m@icloud.com>
2020-07-12 09:49:58 +0900

    コントローラ作成

---
+++ app/Http/Controllers/PokemonController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Pokemon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth; // 認証モジュールの読み込み
+
+class PokemonController extends Controller {
+    // 認証を有効にする
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
+    // 項目マスタ キーとラベル
+    protected $ATTRIBUTES = ['0' => 'ノーマル', '1'=>'ほのお', '2'=>'みず', '3'=>'くさ', '4'=>'でんき', '5'=>'こおり', '6'=>'かくとう', '7'=>'どく', '8'=>'じめん', '9'=>'ひこう', '10'=>'エスパー', '11'=>'むし', '12'=>'いわ', '13'=>'ゴースト', '14'=>'ドラゴン', '15'=>'あく', '16'=>'はがね', '17'=>'フェアリー'];
+    protected $REGIONS = ['a'=>'カントー', 'b'=>'ジョウト', 'c'=>'ホウエン', 'd'=>'シンオウ', 'e'=>'イッシュ', 'f'=>'カロス', 'g'=>'アローラ', 'h'=>'ガラル'];
+
+    public function index() {
+        $user_id = Auth::id(); // ログインユーザIDを取得する
+
+        $query = DB::table('pokemon')->where('user_id', $user_id);
+
+        if (request('attribute')) {
+            $query->whereIn('attribute', request('attribute'));
+        }
+
+        if (request('region')) {
+            $query->whereIn('region', request('region'));
+        }
+
+        $pokemons = $query->orderBy('created_at', 'desc')->get();
+
+        return view('pokemons.index', compact('pokemons'), ['attributes'=>$this->ATTRIBUTES, 'regions'=>$this->REGIONS]);
+    }
+
+    public function create() {
+        return view('pokemons.create', ['attributes'=>$this->ATTRIBUTES, 'regions'=>$this->REGIONS]);
+    }
+
+    public function store(Request $request) {
+        $pokemon = new Pokemon();
+        $pokemon->user_id = Auth::id();
+        $pokemon->name = request('name');
+        $pokemon->attribute = request('attribute');
+        $pokemon->region = request('region');
+        $pokemon->size = request('size');
+        $pokemon->weight = request('weight');
+        $pokemon->attack_name = request('attack_name');
+        if (request('attack_description')) {
+            $pokemon->attack_description = request('attack_description');
+        }
+
+        $pokemon->save();
+        return redirect('pokemons');
+    }
+}
